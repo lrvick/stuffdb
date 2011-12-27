bd7371f073b3ef35e333ca06eac5de694b49279b
lrvick <lance@lrvick.net>
2011-12-26 19:14:43 -0500

    add "list things" view/template

--- main/views.py
+++ main/views.py
@@ -3,23 +3,31 @@ from django.template import RequestContext
 from django.conf import settings
 
 def add_thing(request):
-    title = 'Add Thing'
     return render_to_response(
         'add_thing.html',
         {
             'site_name':settings.SITE_NAME,
-            'title':title,
+            'title':'Add Thing',
         },
         context_instance=RequestContext(request)
     )
 
-def view_thing(request):
-    title = 'View Thing'
+def view_thing(request,slug):
     return render_to_response(
         'view_thing.html',
         {
             'site_name':settings.SITE_NAME,
-            'title':title,
+            'title':'View thing',
+        },
+        context_instance=RequestContext(request)
+    )
+
+def list_things(request):
+    return render_to_response(
+        'view_thing.html',
+        {
+            'site_name':settings.SITE_NAME,
+            'title':'List of things',
         },
         context_instance=RequestContext(request)
     )
--- urls.py
+++ urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
-from main.views import add_thing, view_thing
+from main.views import add_thing, view_thing, list_things
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', add_thing),
+    url(r'^list/$', list_things),
     url(r'^thing/(?P<slug>\w+)$', view_thing),
 )
